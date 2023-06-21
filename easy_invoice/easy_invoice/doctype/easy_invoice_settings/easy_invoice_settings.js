675c95d29964bf0444ae2cd2a62eac48bcb2d6cc
Muhammed Sinan K T <m.sinankt@gmail.com>
2023-06-22 03:32:58 +0530

    feat: settings doctype

---
+++ easy_invoice/easy_invoice/doctype/easy_invoice_settings/easy_invoice_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Muhammed Sinan K T and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Easy Invoice Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
